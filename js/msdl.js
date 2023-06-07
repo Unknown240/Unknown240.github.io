48f9ab0e8a48e8465f289c55ce8f45fc795e9652
Calvin Firlansyah <muhammadlinggar9@gmail.com>
2023-06-07 11:34:16 +0700

    Update msdl.js

--- js/msdl.js
+++ js/msdl.js
@@ -1,5 +1,9 @@
 const langsUrl = "https://www.microsoft.com/en-us/api/controls/contentinclude/html?pageId=cd06bda8-ff9c-4a6e-912a-b92a21f42526&host=www.microsoft.com&segments=software-download%2cwindows11&query=&action=getskuinformationbyproductedition&sdVersion=2";
 const downUrl = "https://www.microsoft.com/en-us/api/controls/contentinclude/html?pageId=cfa9e580-a81e-4a4b-a846-7b21bf4e2e5b&host=www.microsoft.com&segments=software-download%2Cwindows11&query=&action=GetProductDownloadLinksBySku&sdVersion=2";
+const sessionUrl = "https://vlscppe.microsoft.com/fp/tags?org_id=y6jn8c31&session_id="
+
+const apiUrl = "https://massgrave.dev/api/msdl/"
+
 
 const sessionId = document.getElementById('msdl-session-id');
 const msContent = document.getElementById('msdl-ms-content');
